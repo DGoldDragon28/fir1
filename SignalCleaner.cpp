e6825b7dd875f3e3e83dabf4ddafe76c7a885e49
= <rossgardiner24@gmail.com>
2022-03-17 14:21:07 +0000

    adds SNR calculations to SignalCleaner

--- SignalCleaner.cpp
+++ SignalCleaner.cpp
@@ -34,11 +34,35 @@ void SignalCleaner::Filter() {
             float noise_sample = noise_source.samples[channel_idx%nr_noise_channels][sample_idx%len_noise];
             double canceller = internal_fir.filter(noise_sample);
             double output_signal = noisey_sample - canceller;
-            printf("s: %f, n: %f, o: %f \n",noisey_sample, noise_sample, output_signal);
+            //printf("s: %f, n: %f, o: %f \n",noisey_sample, noise_sample, output_signal);
             internal_fir.lms_update(output_signal);
             output.samples[channel_idx][sample_idx] = output_signal;
         }
     }
+}
+
+float SignalCleaner::ComputeRMS(AudioFile<float> signal){
+    float mean_square = 0.0f;
+    for(int channel_idx = 0; channel_idx < signal.getNumChannels(); channel_idx++){
+        for(int sample_idx = 0; sample_idx < signal.getNumSamplesPerChannel(); sample_idx++){
+            mean_square += std::pow(signal.samples[channel_idx][sample_idx], 2);
+        }
+    }
+    return std::sqrt(mean_square);
+}
+
+float SignalCleaner::ComputeSNR(AudioFile<float> signal, AudioFile<float> noise){
+    float signal_rms = ComputeRMS(signal);
+    float noise_rms = ComputeRMS(noise);
+    return signal_rms/noise_rms;
+}
+
+float SignalCleaner::SNRPreFiltered() {
+    return ComputeSNR(noisey_signal, noise_source);
+}
+
+float SignalCleaner::SNRPostFiltered() {
+    return ComputeSNR(output, noise_source);
 }
 
 bool SignalCleaner::SaveFiltered(std::string filename){
--- SignalCleaner.h
+++ SignalCleaner.h
@@ -6,6 +6,8 @@
 #define FIR1_SIGNALCLEANER_H
 #include <Fir1.h>
 #include "AudioFile/AudioFile.h"
+#include <cmath>
+
 
 #define N 100
 
@@ -14,12 +16,16 @@ public:
     SignalCleaner(std::string noisey_signal_path, std::string noise_source_path, unsigned nr_taps, float learning_rate);
     bool SaveFiltered(std::string);
     void Filter();
+    float SNRPreFiltered();
+    float SNRPostFiltered();
 private:
     Fir1 internal_fir;
     AudioFile<float> noisey_signal;
     AudioFile<float> noise_source;
     AudioFile<float> output;
     bool FileExists(std::string filename);
+    float ComputeRMS(AudioFile<float> signal);
+    float ComputeSNR(AudioFile<float> signal, AudioFile<float> noise);
 };
 
 
