8c9b2582d1cfa0697a17de764d92fd60240a2a9a
= <rossgardiner24@gmail.com>
2022-03-31 14:52:43 +0100

    add signal cleaner source

---
+++ SignalCleaner/SignalCleaner.h
@@ -0,0 +1,41 @@
+//
+// Created by ross on 08/03/2022.
+//
+
+#ifndef FIR1_SIGNALCLEANER_H
+#define FIR1_SIGNALCLEANER_H
+#include <Fir1.h>
+#include "AudioFile/AudioFile.h"
+#include <cmath>
+
+
+class SignalCleaner {
+public:
+    AudioFile<double> noisey_signal;
+    AudioFile<double> noise_source;
+    AudioFile<double> output;
+    AudioFile<double> clean;
+
+    SignalCleaner(std::string noisey_signal_path, std::string noise_source_path, unsigned nr_taps, double learning_rate);
+
+    bool SaveFiltered(std::string);
+
+    void Filter();
+
+    double SNRPreFiltered(bool as_db);
+
+    double SNRPostFiltered(bool as_db);
+
+    void PrintSummaries();
+
+    void SetClean(std::string filename);
+
+private:
+    Fir1 internal_fir;
+
+    bool FileExists(std::string filename);
+
+};
+
+
+#endif //FIR1_SIGNALCLEANER_H
