eaaf817e9b7d79d557558b95210b14b3e259e2ec
= <rossgardiner24@gmail.com>
2022-03-31 15:10:21 +0100

    adds audiofile library

---
+++ AudioFile/tests/CMakeFiles/Tests.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ross/CLionProjects/fir1/AudioFile/tests/AiffLoadingTests.cpp" "/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeFiles/Tests.dir/AiffLoadingTests.cpp.o"
+  "/home/ross/CLionProjects/fir1/AudioFile/tests/FileWritingTests.cpp" "/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeFiles/Tests.dir/FileWritingTests.cpp.o"
+  "/home/ross/CLionProjects/fir1/AudioFile/tests/GeneralTests.cpp" "/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeFiles/Tests.dir/GeneralTests.cpp.o"
+  "/home/ross/CLionProjects/fir1/AudioFile/tests/WavLoadingTests.cpp" "/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeFiles/Tests.dir/WavLoadingTests.cpp.o"
+  "/home/ross/CLionProjects/fir1/AudioFile/tests/main.cpp" "/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeFiles/Tests.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PROJECT_BINARY_DIR=\"/home/ross/CLionProjects/fir1/AudioFile/tests\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "tests/doctest"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AudioFile/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ross/CLionProjects/fir1/AudioFile/tests
+# Build directory: /home/ross/CLionProjects/fir1/AudioFile/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Tests "/home/ross/CLionProjects/fir1/AudioFile/tests/Tests")
+set_tests_properties(Tests PROPERTIES  _BACKTRACE_TRIPLES "/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeLists.txt;11;add_test;/home/ross/CLionProjects/fir1/AudioFile/tests/CMakeLists.txt;0;")
