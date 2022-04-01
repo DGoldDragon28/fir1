69209f13e453d24290750851345b38f38c657782
= <rossgardiner24@gmail.com>
2022-04-01 12:42:59 +0100

    correlate docs

--- correlate/correlate.cpp
+++ correlate/correlate.cpp
@@ -1,7 +1,10 @@
 #include "correlate.h"
 #include <sys/stat.h>
 
-
+/// Returns signal to noise ratio (SNR) from an input pearson's correlation coefficient
+/// \param p pearson's correlation coefficient
+/// \param as_db if true, SNR returned in decibels; otherwise returned as numeric ratio.
+/// \return computed SNR
 double wcorr_rho2snr(double p, bool as_db){
     double p_squared = p*p;
     double snr = p_squared / (1 - p_squared);
@@ -10,14 +13,18 @@ double wcorr_rho2snr(double p, bool as_db){
     }
     return snr;
 }
-
+/// calculates pearson's correlation coefficient between inputs x and y
+/// \param x input audio signal x
+/// \param y input audio signal y
+/// \return computed pearson's correlation coefficient
 double wcorr_rho(AudioFile<double> x, AudioFile<double> y){
-    //calculates pearson correlation coeff between inputs x and y
     return wcorr_cov(x, y) / (wcorr_stdev(x) * wcorr_stdev(y));
 }
-
+/// computes the covariance between inputs x and y
+/// \param x input audio signal x
+/// \param y input audio signal y
+/// \return computed covariance
 double wcorr_cov(AudioFile<double> x, AudioFile<double> y){
-    //presumes x and y are the same shape! IE they have the same nr channels
     int n_channels = std::min(x.getNumChannels(), y.getNumChannels());
     int n_samples_per_channel = std::min(x.getNumSamplesPerChannel(), y.getNumSamplesPerChannel());
     double mean_x = wcorr_mean(x);
@@ -30,7 +37,9 @@ double wcorr_cov(AudioFile<double> x, AudioFile<double> y){
     }
     return covariance / (n_samples_per_channel * n_channels);
 }
-
+/// computes the standard deviation of an unput signal
+/// \param signal input audio signal to compute standard deviation
+/// \return standard deviation of input signal
 double wcorr_stdev(AudioFile<double> signal){
     double N = signal.getNumChannels() * signal.getNumSamplesPerChannel();
     double total = 0.0f;
@@ -43,7 +52,9 @@ double wcorr_stdev(AudioFile<double> signal){
     }
     return sqrt(total/N);
 }
-
+/// computes the mean value of a given signal
+/// \param signal input audio signal
+/// \return mean value in the signal
 double wcorr_mean(AudioFile<double> signal){
     //returns mean of a given signal
     double total = 0.0f;
