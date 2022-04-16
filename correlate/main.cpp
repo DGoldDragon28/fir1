866d5b0f795dd516a0ee9ecc9092ba83a16784cc
= <rossgardiner24@gmail.com>
2022-04-16 19:35:48 +0100

    adds working script to filter everything

--- correlate/main.cpp
+++ correlate/main.cpp
@@ -1,30 +1,59 @@
 #include "correlate.h"
 #include <cstdlib>
+#include <cstdio>
 
 int main(int argc, char ** argv) {
     AudioFile<double> f1, f2;
-
+    std::string csv_filename;
+    bool toCSV = false;
+    char  *csv_label;
     switch(argc) {
         case 2:
             f1.load(argv[1]);
             f2.load("/dev/stdin");
             break;
+        case 5:
+            csv_label = argv[4];
+        case 4:
+            toCSV = true;
+            csv_filename=argv[3];
         case 3:
             f1.load(argv[1]);
             f2.load(argv[2]);
             break;
+        
         default:
             std::cerr << "Usage: " << argv[0] << " <file1> [<file2>]" << std::endl;
             return EXIT_FAILURE;
     }
-
-    std::cout << "MEAN1  : " << wcorr_mean(f1) << std::endl;    
-    std::cout << "MEAN2  : " << wcorr_mean(f2) << std::endl;    
-    std::cout << "STDEV1 : " << wcorr_stdev(f1) << std::endl;    
-    std::cout << "STDEV2 : " << wcorr_stdev(f2) << std::endl;    
-    std::cout << "COV    : " << wcorr_cov(f1,f2) << std::endl;    
-    std::cout << "RHO    : " << wcorr_rho(f1,f2) << std::endl;    
-    std::cout << "SNR    : " << wcorr_rho2snr(wcorr_rho(f1,f2), true) << " dB" << std::endl;    
+    
+    double mean_f1 = wcorr_mean(f1);
+    double mean_f2 = wcorr_mean(f2);
+    double stdev_f1 = wcorr_stdev(f1);
+    double stdev_f2 = wcorr_stdev(f2);
+    double cov = wcorr_cov(f1, f2);
+    double rho = wcorr_rho(f1, f2);
+    double snr = wcorr_rho2snr(wcorr_rho(f1, f2), true);
+    
+    if(toCSV){
+        FILE* csv = fopen(csv_filename.c_str(), "a");
+        if(csv){
+            fprintf(csv,"%s,%f,%f,%f,%f,%f,%f,%f,\n", csv_label, mean_f1, mean_f2, stdev_f1, stdev_f2, cov, rho, snr);
+        }
+        else{
+            std::cout << "can't open csv file";
+        }
+    }
+    else{
+        std::cout << "MEAN1  : " << mean_f1 <<  std::endl;
+        std::cout << "MEAN2  : " << mean_f2 << std::endl;
+        std::cout << "STDEV1 : " << stdev_f1 << std::endl;
+        std::cout << "STDEV2 : " << stdev_f2 << std::endl;
+        std::cout << "COV    : " << cov << std::endl;
+        std::cout << "RHO    : " << rho << std::endl;
+        std::cout << "SNR    : " << snr << " dB" << std::endl;
+    }
+    
 }
 
 
--- filter.sh
+++ filter.sh
@@ -1,24 +1,25 @@
 
-flags=
-noisenames=
-signalnames=
-noisedir=
-sigdir=
-dirtydir=
-cleandir=
+flags=""
+noisenames="0010006 07002027 07042249 07069064 07075028 sine50Hz30s sine441Hz30s"
+signalnames="blues.00081 classical.00099 country.00047 disco.00060 hiphop.00061 jazz.00021 metal.00071 pop.00041 reggae.00052 rock.00084"
+noisedir="/home/ross/PycharmProjects/noise-guy/noise-sources"
+sigdir="/home/ross/PycharmProjects/noise-guy/output/upsampled"
+dirtydir="/home/ross/PycharmProjects/noise-guy/output/noisy"
+cleandir="/home/ross/PycharmProjects/noise-guy/output/clean"
+outputcsv="output.csv"
 
 for sig in $signalnames
 do
 	for noise in $noisenames
 	do
-		./signal-cleaner $flags -o "$cleandir/$sig-$noise-f.wav" -f "$noisedir/$noise.wav" "$dirtydir/$sig-$noise.wav"
-		./signal-cleaner $flags -o "$cleandir/$sig-$noise-d.wav" -d "$noisedir/$noise.wav" "$dirtydir/$sig-$noise.wav"
+		./SignalCleaner/signal-cleaner $flags -o "$cleandir/$sig-$noise-f.wav" -f "$noisedir/$noise.wav" "$dirtydir/$sig-$noise.wav"
+		./SignalCleaner/signal-cleaner $flags -o "$cleandir/$sig-$noise-d.wav" -d "$noisedir/$noise.wav" "$dirtydir/$sig-$noise.wav"
 		echo "[Dirty signal '$sig-$noise']"
-	        ./wav-correlate "$sigdir/$sig.wav" "$dirtydir/$sig-$noise.wav"
+	        ./correlate/wav-correlate "$sigdir/$sig.wav" "$dirtydir/$sig-$noise.wav" "$outputcsv" "dirty"
 		echo "['$sig-$noise' filtered by FIR]"
-		./wav-correlate "$sigdir/$sig.wav" "$cleandir/$sig-$noise-f.wav"
+		./correlate/wav-correlate "$sigdir/$sig.wav" "$cleandir/$sig-$noise-f.wav" "$outputcsv" "fir"
 		echo "['$sig-$noise' filtered by DNF]"
-		./wav-correlate "$sigdir/$sig.wav" "$cleandir/$sig-$noise-d.wav"
+		./correlate/wav-correlate "$sigdir/$sig.wav" "$cleandir/$sig-$noise-d.wav" "$outputcsv" "dnf"
 	done
 done
 
